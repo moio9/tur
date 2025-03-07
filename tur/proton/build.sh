ac2b28eeab3a276911cf4a94dad54d7c5d73b57b
airidosas252 <airidaslideikis@gmail.com>
2025-03-07 09:10:35 +0200

    proton: mingw update
    Switch to the newest mingw release by Bylaws.

--- tur/proton/build.sh
+++ tur/proton/build.sh
@@ -77,7 +77,7 @@ enable_wineandroid_drv=no
 _setup_llvm_mingw_toolchain() {
 	# LLVM-mingw's version number must not be the same as the NDK's.
 	local _llvm_mingw_version=19
-	local _version="20240929"
+	local _version="20250228"
 	local _url="https://github.com/bylaws/llvm-mingw/releases/download/$_version/llvm-mingw-$_version-ucrt-ubuntu-20.04-x86_64.tar.xz"
 	local _path="$TERMUX_PKG_CACHEDIR/$(basename $_url)"
 	local _sha256sum=ce75ad076c87663fd4a77513e947252d97ce799a11926c1f3ac7afed1d6ab85c
